8123850488eac4328c6fe53102bc15903f6fd4f5
Tim 'bastelfreak' Meusel <tim@bastelfreak.de>
2014-05-10 02:09:31 +0200

    added gitignore and basic config example

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+$host = '192.168.0.12';
+$user = 'blog';
+$password = 'password';
+$database = 'database';
+
+
+// Some config stuff
+define("NORMAL_CATEGORY", "Allgemein"); # name of the standard category
+define("STATIC_CATEGORY", "STATIC"); # name for dummy category that includes all static sites
+?>
